9250a39b1f0f1153beb2669b1acc779665be5c61
scajasc <sac.cabrera@yavirac.edu.ec>
2020-11-08 17:21:06 -0500

    Particulas Nuevas

--- src/App.js
+++ src/App.js
@@ -70,21 +70,51 @@ class App extends Component {
         <Particles className="particles"
           params={{
             "particles": {
-              "line_linked": {
-                "color": "#FFFFFF"
-              },
               "number": {
-                "value": 150
+                "value": 160,
+                "density": {
+                  "enable": false
+                }
               },
               "size": {
-                "value": 5
+                "value": 3,
+                "random": true,
+                "anim": {
+                  "speed": 4,
+                  "size_min": 0.3
+                }
+              },
+              "line_linked": {
+                "enable": false
+              },
+              "move": {
+                "random": true,
+                "speed": 1,
+                "direction": "top",
+                "out_mode": "out"
               }
             },
             "interactivity": {
               "events": {
                 "onhover": {
                   "enable": true,
+                  "mode": "bubble"
+                },
+                "onclick": {
+                  "enable": true,
                   "mode": "repulse"
+                }
+              },
+              "modes": {
+                "bubble": {
+                  "distance": 250,
+                  "duration": 2,
+                  "size": 0,
+                  "opacity": 0
+                },
+                "repulse": {
+                  "distance": 400,
+                  "duration": 4
                 }
               }
             }
